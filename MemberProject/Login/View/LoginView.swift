896188f0021b8e064612319cf94eee3d2e79878e
wjstjd120 <wjstjd120@naver.com>
2024-11-10 17:41:23 +0900

    UI: SnapKit 추가, 로그인 UI 추가

--- MemberProject/Login/View/LoginView.swift
+++ MemberProject/Login/View/LoginView.swift
@@ -6,18 +6,73 @@
 //
 import UIKit
 
+import SnapKit
+
 class LoginView: UIView {
+    
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private let idTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "아이디"
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "비밀번호"
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        return textField
+    }()
+    
+    private let startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("시작하기", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.layer.masksToBounds = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
-        configureLayout()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    private func configureLayout() {
+    private func configureUI() {
+        self.addSubview(verticalStackView)
         
+        [
+         idTextField,
+         passwordTextField,
+         startButton
+        ].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+        
+//        verticalStackView.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(100)
+//            $0.left.right.equalToSuperview().inset(40)
+//            $0.bottom.equalToSuperview().offset(-100)
+//        }
+        
+        verticalStackView.snp.makeConstraints {
+            $0.width.equalTo(200)
+            $0.height.equalTo(150)
+            $0.center.equalToSuperview()
+        }
     }
 }
